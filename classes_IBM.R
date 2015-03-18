aa8ae553edf30f7c63b303002aecfbf07c15244b
PMahoney29 <pmahoney29@gmail.com>
2015-03-18 16:47:50 -0600

    Added population instantiation with starting values.  Improved genind() functionality

--- classes_IBM.R
+++ classes_IBM.R
@@ -20,20 +20,21 @@ for (c in cols) {
  geninput <- cbind(geninput,paste(datum[,c], datum[,c+1],sep="_")) 
  names(geninput)[ncol(geninput)] <- names(datum)[c]
 }
-genID <- df2genind(geninput[-c(1:2)], sep="_", ind.names=geninput$ID, loc.names=names(geninput[-c(1:2)]), missing = 0, type='codom') 
+genID <- df2genind(geninput[-c(1:2)], sep="_", ind.names=geninput$ID, loc.names=names(geninput[-c(1:2)]), missing = NA, type='codom') 
 mySamp <- genID[sample(1:nrow(genID@tab), 10)]
 mySamp
+genout <- genind2df(genID, oneColPerAll=TRUE)
+write.csv(genout, "startValues.csv")
 
-animID = 'a1'
-sex = "F"
-age = 0
-socialStat = 'Pup'
-reproStat = FALSE
-reproHist = 0
-birthMon = 0
-mortMon = as.numeric(NA)
-liveStat = TRUE
-genotype = data.frame(L1="Aa", L2="cD", stringsAsFactors=FALSE)
+# Test instances of the above two classes
+startValues <- read.csv('./Data//startValues.csv', stringsAsFactors=F)
+genoCols = 10:117; ID = "animID"; sex = 'sex'; age = 'age'; socialStat = 'socialStat'; reproStat = 'reproStat';
+pop1 <- popClass$new(popID = 'Population_1', time=0)
+pop1$startPop(startValues=startValues, ID='animID', sex='sex', age='age', socialStat='socialStat', reproStat='reproStat', genoCols=genoCols)
+
+pop1$tabIndsAll()
+pop1$pullAlive()
+pop1$tabAlive()
 
 
 #####################
@@ -63,7 +64,7 @@ indClass <- setRefClass(
     reproHist = 'numeric',
     liveStat = 'logical',
     birthMon = 'numeric',
-    mortMon = 'numeric',
+    mortMon = 'ANY',         ## Need to fix to be either numeric or NA
     genotype = 'data.frame'))
 
 #,
@@ -78,22 +79,6 @@ indClass <- setRefClass(
 #  )
 
 
-  # Test instances of the above two classes
-a1 <- indClass$new(animID=animID, sex=sex, age=age, socialStat=socialStat, 
-                   reproStat=reproStat, reproHist=reproHist, liveStat=liveStat, birthMon=birthMon, mortMon=mortMon, genotype=genotype)
-a2 <- indClass$new(animID="a2", sex=sex, age=age, socialStat=socialStat, 
-                   reproStat=reproStat, reproHist=reproHist, liveStat=liveStat, birthMon=birthMon, mortMon=mortMon, genotype=genotype)
-a3 <- indClass$new(animID="a3", sex=sex, age=age, socialStat=socialStat, 
-                   reproStat=reproStat, reproHist=reproHist, liveStat=liveStat, birthMon=birthMon, mortMon=mortMon, genotype=genotype)
-pop1 <- popClass$new(popID = 'Population_1', time=0)
-a1$addToPop(pop1)
-a2$addToPop(pop1)
-a3$addToPop(pop1)
-pop1$tabIndsAll()
-pop1$pullAlive()
-pop1$tabAlive()
-
-
 ## indClass methods
   # Print method for individual data
 indClass$methods(tab = function() {
@@ -102,15 +87,14 @@ indClass$methods(tab = function() {
   for (fi in fields) {
     #####  Will need to fix depending on what I decide to do with genind() object classes
     if (class(field(fi))=='data.frame') {
-      g <- c(field(fi), sep = " ")
-      out[1,fi] <- do.call(paste, g)
+      #g <- c(field(fi), sep = " ")
+      #out[1,fi] <- do.call(paste, g)
+      out <- cbind(out, field(fi))
     }
     else {out[1,fi] <- field(fi)}
   }
-  names(out) <- fields
   out
  })
-
 
   # Add method for including individual data to popClass object
 indClass$methods(addToPop = function(popName) {
@@ -125,6 +109,16 @@ indClass$methods(addToPop = function(popName) {
 
 
 ## popClass methods
+popClass$methods(startPop = function(startValues, ID, sex, age, socialStat, reproStat, genoCols) {
+  sv <- startValues
+  for (r in 1:nrow(sv)) {
+    ind <- indClass$new(animID=sv[r,ID], sex=sv[r,sex], age=sv[r,age], socialStat=sv[r,socialStat], reproStat=sv[r,reproStat], reproHist=0, 
+                        liveStat=TRUE, birthMon=0, mortMon=NA, genotype=sv[r,genoCols])
+    ind$addToPop(.self)
+  }
+  .self$pullAlive()
+})
+
   # View individual data (tabulated ~ data.frame)
 popClass$methods(tabIndsAll = function() {
   dat <- field('indsAll')
