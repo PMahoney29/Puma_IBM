d6673a0efbc2efd045b6de352d8b2f84fd072b8c
Peter Mahoney <pmahoney29@gmail.com>
2015-04-12 12:16:01 -0600

    Modified

--- runModel.R
+++ runModel.R
@@ -19,9 +19,9 @@ startValues <- read.csv('./Data/genotypes/startValues_complete.csv', stringsAsFa
 lociNames <- unique(sub("[.].*$","",names(startValues)[-c(1:11)]))
 genoCols = 12:ncol(startValues); startValues$age <- as.numeric(startValues$age); 
 
-pop1 <- popClass$new(popID = 'Population_1', time=0)
-pop1$startPop(startValues=startValues, ID='animID', sex='sex', age='age', mother='mother', father='father',
-              socialStat='socialStat', reproStat='reproStat', genoCols=genoCols)
+#pop1 <- popClass$new(popID = 'Population_1', time=0)
+#pop1$startPop(startValues=startValues, ID='animID', sex='sex', age='age', mother='mother', father='father',
+#              socialStat='socialStat', reproStat='reproStat', genoCols=genoCols)
 
 surv <- read.csv('./Data/survival//survivalMonthly.csv')
 ageTrans <- read.csv('./Data/stageTrans/stageTrans.csv')
@@ -31,17 +31,12 @@ l2 <- litterProbs[1, 'prob']
 l3 <- litterProbs[2, 'prob'] + l2
 l4 <- litterProbs[3, 'prob'] + l3
 probFemaleKitt <- 0.5
+Kf <- 5
+Km <- 2
 
-#pop1$reproduce(l2,l3,l4,probBreed,probFemaleKitt,lociNames)
+sim1 <- simClass$new()
+sim1$startSim(iter = 10, years = 10, startValues = startValues, lociNames = lociNames, genoCols = genoCols, 
+              surv = surv, ageTrans = ageTrans, probBreed = probBreed, litterProbs = litterProbs, probFemaleKitt = probFemaleKitt,
+              Kf = Kf, Km = Km, savePopulations = T, verbose = T)
 
-for (i in 1:25) {
-  pop1$stageAdjust(ageTrans, Kf, Km)
-#  pop1$updateBreedStat()
-#  pop1$reproduce(l2,l3,l4,probBreed,probFemaleKitt,lociNames)
-  pop1$kill(surv)
-  pop1$incremTime()
-#  pop1$updateStats()
-}
-
-pop1$tabAlive()
 
