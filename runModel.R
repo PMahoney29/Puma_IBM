4678b946fbe3b14b6827e9db65ba8e226c676d1e
Peter Mahoney <pmahoney29@gmail.com>
2015-04-19 20:40:57 -0600

    Changed

--- runModel.R
+++ runModel.R
@@ -6,13 +6,15 @@
 ############################################################################
 
 #Load required packages
-library(methods)
-library(adegenet)
-library(plyr)
-library(popbio)
-library(Rhh)
-library(ggplot2)
-#library(PopGenReport)
+#library(methods)
+#library(adegenet)
+#library(plyr)
+#library(popbio)
+#library(Rhh)
+#library(ggplot2)
+#library(parallel)
+#library(foreach)
+#library(doParallel)
 source('classes_IBM.R')
 
 # Test instances of the two classes
@@ -28,15 +30,28 @@ litterProbs$cumProbs <- cumsum(litterProbs$prob)
 probFemaleKitt <- 0.5
 Kf <- 5
 Km <- 2
+#Km <- rbind(c(1, 0.75), c(2, 0.25))
+senesc <- 15
+
+genOutput <- T
+savePopulations <- T
+verbose <- T
+iter = 10
+years = 25
+numCores <- detectCores() - 1
 
 sim1 <- simClass$new()
-sim1$startSim(iter = 10, years = 25, startValues = startValues, lociNames = lociNames, genoCols = genoCols, 
+sim1$startSim(iter = iter, years = years, startValues = startValues, lociNames = lociNames, genoCols = genoCols, 
               surv = surv, ageTrans = ageTrans, probBreed = probBreed, litterProbs = litterProbs, probFemaleKitt = probFemaleKitt,
-              Kf = Kf, Km = Km, senesc = 15, genOutput = T, savePopulations = T, verbose = T)
+              Kf = Kf, Km = Km, senesc = senesc, genOutput = genOutput, savePopulations = savePopulations, verbose = verbose)
+
+sim1$startParSim(numCores = numCores, iter = iter, years = years, startValues = startValues, lociNames = lociNames, genoCols = genoCols, 
+              surv = surv, ageTrans = ageTrans, probBreed = probBreed, litterProbs = litterProbs, probFemaleKitt = probFemaleKitt,
+              Kf = Kf, Km = Km, senesc = senesc, genOutput = genOutput, savePopulations = savePopulations, verbose = verbose)
+
 
 sim1$summary()
 sim1$plot(fieldStat=c('pop.size', 'lambda', 'PropPoly', 'Na', 'Ho', 'He', 'IR', 'Fis'))
 
 # Plot projections
-matplot2(as.matrix(sim1$pop.size[[4]][1:250,]))
-
+matplot2(as.matrix(sim1$pop.size[[4]][1:10,]))
